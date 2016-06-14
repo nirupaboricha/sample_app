013b59818e71d03e451e7755f8e2e8999a9014dc
nirupaboricha <nirupa.boricha@yahoo.com>
2016-06-14 15:46:58 -0700

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
+
